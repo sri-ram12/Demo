3a1561e75f0524c30702f1dd2fe8f46feb6ceb15
sri-ram12 <chandusonti70@gmail.com>
2026-03-26 19:45:48 +0530

    file2 sent successfully

---
+++ file2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is a comment in shell scripting
+set -x
+#echo "My name is Sri Ram"
+#echo "Shell Scriptimg introduced in the year 1971 by Ken Thompson"
+#echo "print the disk space" 
+    df -h
+
+    #echo" print the memory"
+free -g
+#ehco"print the cpu"
+    npro c
